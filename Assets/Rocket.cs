14794cc66f3337c4647bb5f1b98256be9814bca8
hydrictech <hydrictech@gmail.com>
2019-08-03 12:33:44 -0400

    Added Audio-BetaV5

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -8,11 +8,12 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +27,15 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) // so it doesn't layer
+            {
+                audioSource.Play();
+            }
+
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
